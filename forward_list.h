bf47e26878276f4b683c3fa8298ddce59f59641e
Fabryzzio-Meza-Torres <fabryzziomt@gmail.com>
2024-01-09 19:12:12 -0500

    Advance 1

--- forward_list.h
+++ forward_list.h
@@ -35,19 +35,69 @@ public:
         new_Node->next = head;
         head = new_Node;
     }
-    void push_back(T)
-    {
+    void push_back(T value)
+    { // Agrega un elemento al final
+        Node *new_node = new Node;
         while (temp->next != NULL)
         {
             temp = temp->next;
         }
+        temp->next = new_node;
+        new_node->data = value;
+        new_node->next = NULL;
     }
-    T pop_front();     // Remueve el elemento al comienzo
-    T pop_back();      // Remueve el elemento al final
+
+    T pop_front()
+    { // Remueve el elemento al comienzo
+        temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    T pop_back()
+    { // Remueve el elemento al final
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        delete temp;
+    }
+
     T operator[](int); // Retorna el elemento en la posición indicada
-    bool empty();      // Retorna si la lista está vacía o no
-    int size();        // Retorna el tamaño de la lista
-    void clear();      // Elimina todos los elementos de la lista
-    void sort();       // Implemente un algoritmo de ordenacion con listas enlazadas)
-    void reverse();    // Revierte la lista
+
+    bool empty()
+    { // Retorna si la lista está vacía o no
+        if (head == NULL)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    int size()
+    { // Retorna el tamaño de la lista
+        int counter = 0;
+        while (temp->next != NULL)
+        {
+            counter++;
+            temp = temp->next;
+        }
+        return counter;
+    }
+    void clear()
+    { // Elimina todos los elementos de la lista
+        temp = head;
+        while (temp->next != NULL)
+        {
+            delete temp;
+            temp = temp->next;
+        }
+    }
+    void sort()
+    { // Implemente un algoritmo de ordenacion con listas enlazadas)
+    }
+    void reverse(); // Revierte la lista
 };
