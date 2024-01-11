85ce6326567d2c108502d81bf62dad9ce368bdf1
Fabryzzio-Meza-Torres <fabryzziomt@gmail.com>
2024-01-10 23:24:05 -0500

    Main update

--- Doubly_Linked_List.h
+++ Doubly_Linked_List.h
@@ -1,161 +1,208 @@
 #include <iostream>
 using namespace std;
-template<class T>
-struct NodeDouble {
+template <class T>
+struct NodeDouble
+{
     T data;
-    NodeDouble * next=nullptr;
-    NodeDouble * prev=nullptr;
+    NodeDouble *next = nullptr;
+    NodeDouble *prev = nullptr;
 };
-template<class T>
-class List{
+template <class T>
+class List
+{
 private:
-    NodeDouble <T>* head;
-    NodeDouble <T>* tail;
+    NodeDouble<T> *head;
+    NodeDouble<T> *tail;
+
 public:
-    List(): head(nullptr), tail(nullptr){}
-    T front(){
+    List() : head(nullptr), tail(nullptr) {}
+    T front()
+    {
         return head->data;
     };
-    T back(){
+    T back()
+    {
         return tail->data;
     };
-    void push_front(T value){
-        NodeDouble <T>* temp = new NodeDouble <T>{value,head};
-        temp->data=value;
-        if(head == nullptr){
+    void push_front(T value)
+    {
+        NodeDouble<T> *temp = new NodeDouble<T>{value, head};
+        temp->data = value;
+        if (head == nullptr)
+        {
             head = temp;
             tail = temp;
-        }else{
-            head->prev=temp;
-            temp->next=head;
-            head=temp;
+        }
+        else
+        {
+            head->prev = temp;
+            temp->next = head;
+            head = temp;
         }
     };
-    void push_back(T value){
-        NodeDouble <T>* temp = new NodeDouble <T>{value,nullptr};
-        temp->data=value;
-        if(tail == nullptr){
+    void push_back(T value)
+    {
+        NodeDouble<T> *temp = new NodeDouble<T>{value, nullptr};
+        temp->data = value;
+        if (tail == nullptr)
+        {
             tail = temp;
             head = temp;
-        }else{
+        }
+        else
+        {
             tail->next = temp;
             temp->prev = tail;
             tail = temp;
         }
     };
-    T pop_front(){
-        if (empty()){
+    T pop_front()
+    {
+        if (empty())
+        {
             return 0;
         }
-        else{
-            NodeDouble <T> *temp= head;
+        else
+        {
+            NodeDouble<T> *temp = head;
             head = head->next;
-            head->prev=nullptr;
-            temp->next=nullptr;
+            head->prev = nullptr;
+            temp->next = nullptr;
             delete temp;
             return 0;
         }
     };
-    T pop_back(){
-        if (empty()){
+    T pop_back()
+    {
+        if (empty())
+        {
             return 0;
         }
-        else{
-            NodeDouble <T>*temp=tail;
+        else
+        {
+            NodeDouble<T> *temp = tail;
             tail = tail->prev;
             tail->next = nullptr;
-            temp->prev=nullptr;
+            temp->prev = nullptr;
             delete temp;
             return 0;
         }
     };
-    void insert(T value ,int pos){
-        int cont=0;
-        NodeDouble <T>* node = new NodeDouble <T>;
-        node->data=value;
-        NodeDouble <T>* temp=head;
-        if(pos==0){
+    void insert(T value, int pos)
+    {
+        int cont = 0;
+        NodeDouble<T> *node = new NodeDouble<T>;
+        node->data = value;
+        NodeDouble<T> *temp = head;
+        if (pos == 0)
+        {
             push_front(value);
             return;
         }
-        else if(pos==size()){
+        else if (pos == size())
+        {
             push_back(value);
             return;
         }
-        else if(pos>size()){
-            cout<<"Posicion no valida"<<endl;
+        else if (pos > size())
+        {
+            cout << "Posicion no valida" << endl;
             return;
         }
-        while(cont++<pos-1){
-            temp=temp->next;
+        while (cont++ < pos - 1)
+        {
+            temp = temp->next;
         }
-        node->next=temp->next;
-        node->prev=temp;
-        temp->next->prev=node;
-        temp->next=node;
+        node->next = temp->next;
+        node->prev = temp;
+        temp->next->prev = node;
+        temp->next = node;
     };
-    void remove(int pos){
-        int cont=0;
-        NodeDouble <T>* temp=head;
-        if(pos==0){
+    void remove(int pos)
+    {
+        int cont = 0;
+        NodeDouble<T> *temp = head;
+        if (pos == 0)
+        {
             pop_front();
             return;
         }
-        else if(pos==size()-1){
+        else if (pos == size() - 1)
+        {
             pop_back();
             return;
         }
-        else if(pos>size()){
-            cout<<"Posicion no valida"<<endl;
+        else if (pos > size())
+        {
+            cout << "Posicion no valida" << endl;
             return;
         }
-        else{
-            while(cont++<pos){
-                temp=temp->next;
+        else
+        {
+            while (cont++ < pos)
+            {
+                temp = temp->next;
             }
-            if(temp->next){
+            if (temp->next)
+            {
                 temp->next->prev = temp->prev;
             }
-            if(!temp->prev){
+            if (!temp->prev)
+            {
                 head = temp->next;
-            }else{
-                temp->prev->next = temp-> next;
+            }
+            else
+            {
+                temp->prev->next = temp->next;
             }
             delete temp;
         }
-
     };
-    T operator[ ](int pos){
-        int cont=0;
-        NodeDouble <T>* temp=head;
-        while(cont++<pos){
-            temp=temp->next;
+    T operator[](int pos)
+    {
+        int cont = 0;
+        NodeDouble<T> *temp = head;
+        while (cont++ < pos)
+        {
+            temp = temp->next;
         }
         return temp->data;
     };
-    bool empty(){
-        if(head!=nullptr && tail !=nullptr){return false;}
-        else{return true;}
+    bool empty()
+    {
+        if (head != nullptr && tail != nullptr)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     };
-    int size(){
-        int cont=0;
-        NodeDouble <T>* temp;
+    int size()
+    {
+        int cont = 0;
+        NodeDouble<T> *temp;
         temp = head;
-        if (empty()){
+        if (empty())
+        {
             return cont;
         }
-        else{
-            while(temp !=nullptr){
+        else
+        {
+            while (temp != nullptr)
+            {
                 cont++;
-                temp=temp->next;
+                temp = temp->next;
             }
             return cont;
         }
-
     };
-    void clear(){
-        NodeDouble <T>* temp;
-        while(tail != NULL){
+    void clear()
+    {
+        NodeDouble<T> *temp;
+        while (tail != NULL)
+        {
             temp = tail;
             tail = tail->prev;
             delete temp;
@@ -163,26 +210,32 @@ public:
         tail = NULL;
         head = NULL;
     }
-    void reverse(){
-        NodeDouble <T>* temp;
-        NodeDouble <T>* temp1 = tail->next;
+    void reverse()
+    {
+        NodeDouble<T> *temp;
+        NodeDouble<T> *temp1 = tail->next;
         head = tail;
         temp = tail;
-        while(temp != NULL){
+        while (temp != NULL)
+        {
             temp->next = tail->prev;
             temp->prev = temp1;
             temp = temp->next;
         }
-
     }
-    void print() {
-        NodeDouble <T>* temp;
+    void print()
+    {
+        NodeDouble<T> *temp;
         temp = head;
-        if(head ==NULL){
-            cout<<"Lista vacia"<<endl;}
-        while (temp != nullptr) {
+        if (head == NULL)
+        {
+            cout << "Lista vacia" << endl;
+        }
+        while (temp != nullptr)
+        {
             cout << temp->data << " ";
             temp = temp->next;
         }
+        cout << endl;
     }
 };
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     // FowardList:
-
+    cout << "FowardList tests:" << endl;
     ForwardList<int> Listagod;
     Listagod.push_front(7);
     Listagod.push_front(1);
@@ -34,33 +34,34 @@ int main()
     Listagod.print();
     Listagod.clear();
     Listagod.print();
+    cout << endl;
 
     // Doubly Linked List
+    cout << "Doubly Linked List tests:" << endl;
     List<int> prueba;
     prueba.push_front(5);
     prueba.push_front(7);
     prueba.push_front(16);
     prueba.push_back(17);
     prueba.print();
-    cout << endl;
+
     cout << prueba.front() << endl;
     cout << prueba.back() << endl;
     prueba.pop_back();
     prueba.print();
-    cout << endl;
     prueba.pop_front();
     prueba.print();
-    cout << endl;
+
     prueba.insert(2, 1);
     prueba.print();
-    cout << endl;
     prueba.remove(1);
     prueba.print();
-    cout << endl;
     cout << prueba[1] << endl;
     cout << prueba.empty() << endl;
     cout << prueba.size() << endl;
     prueba.reverse();
     prueba.print();
+
     prueba.clear();
+    prueba.print();
 }
